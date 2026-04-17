15ab93df3cd86407a6fc74d10e5c2441fc3bbc2d
linlingwanqing-commits <linlingwanqing@gmail.com>
2026-04-18 01:02:17 +0800

    ggg

--- qmk_porting/keyboards/planck59/keymaps/default/keymap.c
+++ qmk_porting/keyboards/planck59/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(/* 0: qwerty */
                      KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, KC_BSPC,
