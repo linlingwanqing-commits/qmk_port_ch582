83c624cb65707f48bf12ced310f9b87a766d721a
linlingwanqing-commits <linlingwanqing@gmail.com>
2026-04-17 17:52:23 +0800

    ff

--- qmk_porting/keyboards/planck59/qmk_config.h
+++ qmk_porting/keyboards/planck59/qmk_config.h
@@ -26,15 +26,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      planck59
 
 /* 矩阵配置 */
+/* 矩阵配置 */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 13
-#define MATRIX_ROW_PINS         \
-    {                           \
-        B18, B19, B20, B21, B23   \
+#define MATRIX_ROW_PINS \
+    { \
+        A2, A3, A4, A5, A6 \
     }
-#define MATRIX_COL_PINS                                                                                \
-    {                                                                                                  \
-        B17, A8, A10, A12, A14, A3, A1, A6, A4, B16, A9, A7, A11 \
+#define MATRIX_COL_PINS \
+    { \
+        B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12 \
     }
 
 #define DIODE_DIRECTION  COL2ROW
