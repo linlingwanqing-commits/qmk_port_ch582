e76799c72b732ee880ddcee174fc4964dd52fa41
linlingwanqing-commits <linlingwanqing@gmail.com>
2026-04-17 18:10:26 +0800

    hhh

--- qmk_porting/keyboards/planck59/halconf.h
+++ qmk_porting/keyboards/planck59/halconf.h
@@ -47,3 +47,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define BLE_TX_POWER LL_TX_POWEER_4_DBM
 // #define BLE_TX_POWER LL_TX_POWEER_5_DBM
 // #define BLE_TX_POWER LL_TX_POWEER_6_DBM
+#pragma once
+
+// #define DEBUG                Debug_UART1
+#define DEBUG_BAUDRATE       460800
+#define DCDC_ENABLE          1
+#define FREQ_SYS             40000000
+#define LSE_ENABLE           1
+#define BLE_SLOT_NUM         4
+#define HSE_LOAD_CAPACITANCE 20 // in pF unit
+#define LSE_LOAD_CAPACITANCE 19 // in pF unit
